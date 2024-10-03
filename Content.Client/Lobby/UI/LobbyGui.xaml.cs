25434a53c3a7fd57637044a43875ed90e4f01427
AgentePanela <agentepanela@gmail.com>
2024-10-03 18:14:04 -0300

    admin remarks button

--- Content.Client/Lobby/UI/LobbyGui.xaml.cs
+++ Content.Client/Lobby/UI/LobbyGui.xaml.cs
@@ -23,6 +23,7 @@ namespace Content.Client.Lobby.UI
             LobbySong.SetMarkup(Loc.GetString("lobby-state-song-no-song-text"));
 
             LeaveButton.OnPressed += _ => _consoleHost.ExecuteCommand("disconnect");
+            RemarksButton.OnPressed += _ => _consoleHost.ExecuteCommand("adminremarks");
             OptionsButton.OnPressed += _ => _userInterfaceManager.GetUIController<OptionsUIController>().ToggleWindow();
         }
 
